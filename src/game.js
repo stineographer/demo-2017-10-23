e1d0b016db656f6abb6ebea49dcf1d795bfc6230
Christine Nicole Gibson <christine.metamoment@gmail.com>
2018-10-14 20:51:14 -0400

    Preliminary Demonstration of Amoeba Thigmotropism
    -very basic demo of Amoeba reaction to "enemy"

--- src/game.js
+++ src/game.js
@@ -1,21 +1,22 @@
 const canvas = document.getElementById('stage')
 , stage = canvas.getContext('2d')
-, sw = 640
-, sh = 640
+, stageWidth = 640
+, stageHeight = 640
 , buttons = {
     Left: 0,
     Right: 0,
     Up: 0,
     Down: 0
 }
-, cellW = 16
-, cellH = 16
+, cellWidth = 16
+, cellHeight = 16
 
 let then = Date.now()
 
-canvas.width = sw
-canvas.height = sh
+canvas.width = stageWidth
+canvas.height = stageHeight
 
+//setting background colour
 stage.fillColor = 'black'
 stage.fillRect(0, 0, canvas.width, canvas.height)
 
@@ -26,18 +27,19 @@ const btn = name => name in buttons && buttons[name]
 const clamp = (min, max, v) =>
       v <= min ? min : v >= max ? max : v
 
-const grid = (w, h, fill=0) => {
+const grid = (width, height, fill=0) => {
+  //this grid is used to create lines in the Field
     const arr = []
-    for (let j = 0; j < h; j++) {
-        arr.push(Array(w).fill(fill))
+    for (let j = 0; j < height; j++) {
+        arr.push(Array(width).fill(fill))
     }
     return arr
 }
 
-const Field = (w, h, offsetX=0, offsetY=0) => ({
+const Field = (width, height, offsetX=0, offsetY=0) => ({
     level: 0,
-    lines: grid(w, h),
-    w, h,
+    lines: grid(width, height),
+    width, height,
     offsetX,
     offsetY
 })
@@ -48,9 +50,10 @@ const Amoeba = (x, y, sizeFactor=2, color='yellow') => ({
   sizeFactor: sizeFactor,
   centreX: x*sizeFactor,
   centreY: y*sizeFactor,
+  maxX: x*sizeFactor*2,
+  maxY: y*sizeFactor*2,
   color
 })
-
 
 
 const Other = (x, y, dx, dy, color='pink') => ({
@@ -74,21 +77,30 @@ const update = dt => {
     if (btn('Up')) dy = -1
     if (btn('Down')) dy = 1
 
-
     if (currentTick >= 0.75) {
         for (const enemy of enemies) {
             enemy.x += enemy.dx
             enemy.y += enemy.dy
+
+            // Amoeba boundary check!
+            if(enemy.x >= currentAmoeba.startX && enemy.y >= currentAmoeba.startY){
+              //Amoeba breach protocol!
+              currentAmoeba.color='red'
+              //begin evasive action!
+              currentAmoeba.startX += currentAmoeba.sizeFactor
+              currentAmoeba.startY -= currentAmoeba.sizeFactor
+            }
+
             if (enemy.x < 0 ||
-                enemy.x > currentField.w - 1 ||
+                enemy.x > currentField.width - 1 ||
                 enemy.y < 0 ||
-                enemy.y > currentField.h - 1)
+                enemy.y > currentField.height - 1)
                 enemy.remove = true
         }
         currentTick = 0
     }
 
-    console.log(enemies)
+    console.log("Amoeba start coordinates", currentAmoeba.startX, currentAmoeba.startY)
     enemies = enemies.filter(e => e.remove === false)
 
     currentTick += dt
@@ -96,45 +108,49 @@ const update = dt => {
 
 const render = () => {
     stage.fillStyle = 'black'
-    stage.fillRect(0, 0, sw, sh)
+    stage.fillRect(0, 0, stageWidth, stageHeight)
 
-    for (let j = 0; j < currentField.h; j++) {
+    for (let j = 0; j < currentField.height; j++) {
 
-        for (let i = 0; i < currentField.w; i++) {
+        for (let i = 0; i < currentField.width; i++) {
             stage.fillStyle = '#333'
-            stage.fillRect((i * (cellW + 2)) + currentField.offsetX,
-                           (j * (cellH + 2)) + currentField.offsetY,
-                           cellW, cellH)
+            stage.fillRect((i * (cellWidth + 2)) + currentField.offsetX,
+                           (j * (cellHeight + 2)) + currentField.offsetY,
+                           cellWidth, cellHeight)
         }
     }
 
-    for (let j = 0; j < currentField.h; j++) {
+    for (let j = 0; j < currentField.height; j++) {
 
-        for (let i = 0; i < currentField.w; i++) {
-
+        for (let i = 0; i < currentField.width; i++) {
+          //when i and j reach Amoeba's starting coordinates
           if (i === currentAmoeba.startX && j === currentAmoeba.startY) {
-
+            //this is what draws the Amoeba
                 stage.fillStyle = currentAmoeba.color
+                // cellX and cellY are the starting points
+                //where colour will be filled in for Amoeba
+                const cellX = (i * (cellWidth + 2)) + currentField.offsetX
+                , cellY = (j * (cellHeight + 2)) + currentField.offsetY
 
-                const cellX = (i * (cellW + 2)) + currentField.offsetX
-                , cellY = (j * (cellH + 2)) + currentField.offsetY
                 stage.fillRect(cellX + 0, cellY + 0,
-                               cellW + currentAmoeba.centreX*currentAmoeba.sizeFactor,
-                               cellH + currentAmoeba.centreY*currentAmoeba.sizeFactor)
+                               cellWidth + currentAmoeba.centreX*currentAmoeba.sizeFactor,
+                               cellHeight + currentAmoeba.centreY*currentAmoeba.sizeFactor)
             }
 
         }
     }
 
     for (const enemy of enemies) {
+      //this is what colors in the enemies
+      stage.fillStyle = enemy.color
+      
+       const cellX = (enemy.x * (cellWidth + 2)) + currentField.offsetX
+       , cellY = (enemy.y * (cellHeight + 2)) + currentField.offsetY
 
-       const cellX = (enemy.x * (cellW + 2)) + currentField.offsetX
-       , cellY = (enemy.y * (cellH + 2)) + currentField.offsetY
-       stage.fillStyle = enemy.color
        stage.fillRect(cellX + 14, cellY + 14,
-                      cellW - 1, cellH - 1)
+                      cellWidth - 1, cellHeight - 1)
    }
-}
+}//end of render
 
 const loop = () => {
     const now = Date.now()
